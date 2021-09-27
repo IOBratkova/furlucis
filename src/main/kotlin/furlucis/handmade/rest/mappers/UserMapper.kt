cbae7aac11992616d747ac6de373aa92d95c95c1
Irina Bratkova <bratkova_irol@edu.bstu.ru>
2021-09-27 20:52:53 +0300

    added users service and mappers

--- src/main/kotlin/furlucis/handmade/rest/mappers/UserMapper.kt
+++ src/main/kotlin/furlucis/handmade/rest/mappers/UserMapper.kt
@@ -1,15 +1,17 @@
 package furlucis.handmade.rest.mappers
 
-import furlucis.handmade.rest.dto.RegisterRequestDto
-import furlucis.handmade.entity.UserCredentials
 import furlucis.handmade.entity.UserInfo
-import furlucis.handmade.enums.RoleEnum
 import furlucis.handmade.rest.dto.UserInfoDto
+import org.mapstruct.InheritInverseConfiguration
 import org.mapstruct.Mapper
 import org.mapstruct.Mapping
 
 @Mapper(componentModel = "spring")
 interface UserMapper {
+
     @Mapping(source = "userCredentials", target = "userCredentials.id")
     fun toUserInfo(userInfoDto: UserInfoDto) : UserInfo
+
+    @InheritInverseConfiguration
+    fun toUserInfoDto(userInfo: UserInfo) : UserInfoDto
 }
